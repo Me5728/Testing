741ab2039bf01b48c68bb69ea3e2c3f43f2e5c94
Me5728 <sg5857@pleasantonusd.net>
2025-07-31 17:40:43 -0700

    Update main.py
    Reinserting missing lines of code (it only committed the starter code apparently)

--- main.py
+++ main.py
@@ -7,85 +7,67 @@ pd.set_option('max_colwidth', None)
 print("\n-----\n")
 
 #Create a variable to read the dataset
-
-
-print(
-    "We will be performing data analysis on this Indicators of Heart Disease Dataset. Here is a sample of it: \n"
-)
+df = pd.read_csv("heartDisease_2020_sampling.csv")
 
 #Print the dataset's first five rows
-
+print("We will be performing data analysis on this Indicators of Heart Disease Dataset. Here is a sample of it: \n")
+print(df.head())
 
 input("\n Press Enter to continue.\n")
 
-
-
 #Data Cleaning
 #Label encode the dataset
-
-
+df = util.labelEncoder(df, ["HeartDisease", "Smoking", "AlcoholDrinking", "AgeCategory", "PhysicalActivity", "GenHealth"])
 print("\nHere is a preview of the dataset after label encoding. \n")
-
+print(df.head())
 
 input("\nPress Enter to continue.\n")
 
 #One hot encode the dataset
-
-
-print(
-    "\nHere is a preview of the dataset after one hot encoding. This will be the dataset used for data analysis: \n"
-)
-
+df = util.oneHotEncoder(df, ["Sex", "Race"])
+print("\nHere is a preview of the dataset after one hot encoding. This will be the dataset used for data analysis: \n")
+print(df.head())
 
 input("\nPress Enter to continue.\n")
-
-
 
 #Creates and trains Decision Tree Model
 from sklearn.model_selection import train_test_split
-
-
-
-
+X = df.drop("HeartDisease", axis = 1)
+y = df["HeartDisease"] 
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 from sklearn.tree import DecisionTreeClassifier
-
-
-
-
-
+dt = DecisionTreeClassifier(max_depth = 8, class_weight = "balanced", random_state = 6)
+dt.fit(X_train, y_train)
 
 #Test the model with the testing data set and prints accuracy score
-
+y_pred = dt.predict(X_test)
 
 from sklearn.metrics import accuracy_score
-
-
-
+test_acc = accuracy_score(y_test, y_pred)
+print("The accuracy with the testing dataset of the Decision Tree is: " + str(test_acc))
 
 #Prints the confusion matrix
 from sklearn.metrics import confusion_matrix
-
-
-
+print("The confusion matrix of the tree is: ")
+print(confusion_matrix(y_test, y_pred, labels = [1, 0]))
 
 
 #Test the model with the training data set and prints accuracy score
-
-
-
+ytrain_pred = dt.predict(X_train)
+train_acc = accuracy_score(y_train, ytrain_pred)
+print()
+print()
+print("The accuracy with the training dataset of the Decision Tree is: " + str(train_acc))
 
 input("\nPress Enter to continue.\n")
 
-
-
 #Prints another application of Decision Trees and considerations
 print("\nBelow is another application of decision trees and considerations for using them:\n")
-
-
-
-
+print("Decision Trees can be incredibly useful machine learning tools and are able to be employed in a variety of fields to great success, including (but not limited to) sustainable development on our planet. With the help of these models, we can potentially analyze species vulnerability based on external factors.")
+print("But when creating that Decision Tree, one would have to ensure that label encoding is used only where necceseary to avoid order bias. It would also be prudent to ensure that one works with a balanced datset.")
 
 #Prints a text representation of the Decision Tree
 print("\nBelow is a text representation of how the Decision Tree makes choices:\n")
 input("\nPress Enter to continue.\n")
+util.printTree(dt, X.columns)
